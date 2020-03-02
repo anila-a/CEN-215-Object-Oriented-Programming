c9deef4e0c3d7e78115ff7b9d6b0a4305eb4720a
Anila Hoxha <51361033+anila-a@users.noreply.github.com>
2020-03-02 18:55:07 +0100

    Update rectangle.java

--- lab03/rectangle.java
+++ lab03/rectangle.java
@@ -1,3 +1,14 @@
+/* Design a class named Rectangle to represent a rectangle. The class contains:
+1-Two double data fields named width and height that specify the width and height of the rectangle. The default
+values are 1 for both width and height .
+2-A no-arg constructor that creates a default rectangle.
+3-A constructor that creates a rectangle with the specified width and height .
+4-A method named getArea() that returns the area of this rectangle.
+5-A method named getPerimeter() that returns the perimeter.
+
+Implement the class. Write a test program that creates two Rectangle objects—one with width 4 and height 40 and the
+other with width 3.5 and height 35.9 . Display the width, height, area, and perimeter of each rectangle in this order. */
+
 package lab3_assignments;
 
 public class rectangle {
