08baa9600cc7313ad4978a8e5ca61c7eeb3d7793
Anila Hoxha <51361033+anila-a@users.noreply.github.com>
2020-03-02 18:53:04 +0100

    Update myInteger.java

--- lab03/myInteger.java
+++ lab03/myInteger.java
@@ -1,3 +1,18 @@
+/* Design a class named MyInteger. The class contains:
+An int data field named value that stores the int value represented by this object.
+A constructor that creates a MyInteger object for the specified int value.
+A get method that returns the int value.
+Methods isEven(), isOdd(), and isPrime() that return true if the value is even, odd, or prime, respectively.
+Static methods isEven(int), isOdd(int), and isPrime(int) that return true if the specified value is even, odd, or prime, respectively.
+Static methods isEven(MyInteger), isOdd(MyInteger), and isPrime(MyInteger) that return true if the specified value is
+even, odd, or prime, respectively.
+Methods equals(int) and equals(MyInteger) that return true if the value in the object is equal to the specified value.
+A static method parseInt(char[]) that converts an array of numeric characters to an int value.
+A static method parseInt(String) that converts a string into an int value. Draw the UML diagram for the class. Implement the
+class. 
+
+Write a client program that tests all methods in the class. */
+
 package lab3_assignments;
 
 public class myInteger {
