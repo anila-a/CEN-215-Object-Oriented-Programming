f9b0f70becec16ad49b42bcbeaa0a4ee91e30e23
Anila Hoxha <51361033+anila-a@users.noreply.github.com>
2020-03-02 19:15:54 +0100

    Update Students1.java

--- lab06/Students1.java
+++ lab06/Students1.java
@@ -7,7 +7,7 @@ import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.util.Scanner;
 
-public class Students {
+public class Students1 {
 
 	public static void main(String[] args) {
 		Student[] s = new Student[10];
