7a862b4e777ed06d037aa22357a3c695c5d41ce3
Anila Hoxha <51361033+anila-a@users.noreply.github.com>
2020-03-02 17:24:38 +0100

    Create welcome.java

---
+++ welcome.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class Welcome {
+	public static void main(String[] args) {
+		System.out.println("Welcome to CEN215!");
+		System.out.println("Welcome!");
+		JOptionPane.showMessageDialog(null, "Java");
+	}
+}
