3ec09f32a2f231d82a0c8a5d5bbfd3b5215aa263
Anila Hoxha <51361033+anila-a@users.noreply.github.com>
2020-03-02 17:37:14 +0100

    Create reverse.java

---
+++ lab02/reverse.java
@@ -0,0 +1,25 @@
+/* Write a program that
+reads ten integers and displays them in the reverse of the
+order in which they were read. */
+
+package lab2_assignment;
+import java.util.Scanner;
+
+public class ex5 {
+
+	public static void main(String[] args) {
+		int[] num = new int[10];
+		
+		for(int x = 0; x < 10; x++) {
+			Scanner sc = new Scanner(System.in);
+			int i =  sc.nextInt();
+			
+			num[x] = i;		
+		}
+		
+		for(int x = 9; x >= 0; x--) {
+			System.out.print(num[x]);
+		}
+	}
+
+}
