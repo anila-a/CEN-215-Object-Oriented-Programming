06d72371f8067b960416901582e78a9fe7c22bc4
Anila Hoxha <51361033+anila-a@users.noreply.github.com>
2020-03-02 17:41:41 +0100

    Update pentagonal.java

--- lab02/pentagonal.java
+++ lab02/pentagonal.java
@@ -1,3 +1,13 @@
+/* A pentagonal number is defined as n(3n–1)/2 for . . ., and so on. 
+Therefore, the first few numbers are 1, 5, 12, 22, . . .. 
+
+Write a method with the following header that returns a pentagonal number:
+
+public static int getPentagonalNumber(int n)
+
+Write a test program that uses this method to display the first
+100 pentagonal numbers with 10 numbers on each line. */
+
 package lab2_assignment;
 
 public class pentagonal {
